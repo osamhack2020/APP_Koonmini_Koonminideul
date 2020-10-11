4e7a0df966518fdef4d5007f3e91988d214ac9a6
daisophila <daisophila@gmail.com>
2020-10-11 14:26:10 +0900

    refresh

--- ScreenLockTest/app/src/main/java/com/example/screenlocktest/MainActivity.java
+++ ScreenLockTest/app/src/main/java/com/example/screenlocktest/MainActivity.java
@@ -2,39 +2,13 @@ package com.example.screenlocktest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button onBtn, offBtn;
-    public Intent intent2 = new Intent(getApplicationContext(), LockScreenActivity.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        onBtn = findViewById(R.id.b1);
-        offBtn = findViewById(R.id.b2);
-
-        onBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(intent2);
-                Intent intent = new Intent(getApplicationContext(), ScreenService.class);
-                startService(intent);
-            }
-        });
-
-        offBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ScreenService.class);
-                stopService(intent);
-            }
-        });
     }
 }
