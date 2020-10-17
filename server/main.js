d0fca54935c5ae8fc2fa6c9ac44d473cecb597e5
ASak1104 <khw56184@kookmin.ac.kr>
2020-10-17 21:52:06 +0900

    Test default document

--- server/main.js
+++ server/main.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
 
 app.get('/', (req, res) => {
-    res.send('Hello Koonmini!')
+    fs.readFile('index.html', 'utf8', (err, html) => {
+        res.send(html)
+    })
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
