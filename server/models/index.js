d2d7fa4b9d5a64f0b704e316cc413a104c59486e
ASak1104 <khw56184@kookmin.ac.kr>
2020-10-25 02:03:41 +0900

    Add sequelize for mysql and create .gitignore

---
+++ server/models/index.js
@@ -0,0 +1,13 @@
+
+const Sequelize = require('sequelize');
+
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+
+db.Sequelize = Sequelize;
+
+module.exports = db;
