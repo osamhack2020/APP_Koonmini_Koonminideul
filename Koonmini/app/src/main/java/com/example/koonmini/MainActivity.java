d45b55a2aa22b3ce4705ab8ef186e4ff9a1a7e10
daisophila <daisophila@gmail.com>
2020-10-25 09:24:18 +0900

    잠금화면 디자인 패치

--- Koonmini/app/src/main/java/com/example/koonmini/MainActivity.java
+++ Koonmini/app/src/main/java/com/example/koonmini/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -55,7 +54,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-
-        textView2.setText(goOut);
+        if(goOut != null)
+            textView2.setText(goOut + " 중");
+        else
+            textView2.setText("");
     }
 }
