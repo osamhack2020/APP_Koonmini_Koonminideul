68a2d4aa7b03c109c33ac616a199780360567f6e
ASak1104 <khw56184@kookmin.ac.kr>
2020-10-28 14:25:23 +0900

    Add modules

--- server/app.js
+++ server/app.js
@@ -1,35 +1,73 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const nunjucks = require('nunjucks');
+const helmet = require('helmet');
+const compression = require('compression');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+dotenv.config();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+// const sequelize = require('./models/index').sequelize;
+const { sequelize } = require('./models')
 
-app.set('views', __dirname + '/views');
+const app = express();
 
-app.engine('html', require('ejs').renderFile);
+app.use(helmet())
+app.use(compression())
+
 app.set('view engine', 'html');
+nunjucks.configure('views', {
+    express: app,
+    watch: true,
+})
 
-app.use(logger('dev'));
+
+sequelize.sync({ force: false})
+    .then(() => {
+        console.log('Database connect complete')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+    name: 'session-cookie',
+}))
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// app.use(function(req, res, next) {
-//     res.status(404).send('Sorry cant find that!');
-// });
-//
-// app.use(function (err, req, res, next) {
-//     console.error(err.stack)
-//     res.status(500).send('Something broke!')
-// });
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(function(req, res, next) {
+    res.status(404).redirect('/error-404.html');
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).redirect('/error-500.html')
+});
 
 module.exports = app;
 
--- server/models/index.js
+++ server/models/index.js
@@ -1,13 +1,12 @@
-const path = require('path');
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
-const config = require(
-    path.join(__dirname + '..', 'config', 'config.json')
-)[env];
+const config = require('../config/config.json')[env];
 const db = {};
-const sequelize = new Sequelize(
-    config.database, config.username, config.password, config
-);
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+
+// db.User = require('./user')(sequelize, Sequelize);
+
 module.exports = db;
