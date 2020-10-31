6be803e46549ff2ff8436273e245d03a0ad6826e
ASak1104 <khw56184@kookmin.ac.kr>
2020-10-31 22:04:00 +0900

    Modify manage user router

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 // const db = require('../models')
-const { Op } = require('sequelize');
+// const { Op } = require('sequelize');
 const { Admin, User } = require('../models');
 
 
--- server/routes/manage.js
+++ server/routes/manage.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { Op } = require('sequelize');
+const { Admin, User } = require('../models');
 
 
 /* GET manage/user page. */
-router.get('/user', (req, res, next) => {
+router.get('/user', async (req, res, next) => {
+    let pageId = -1;
+    if (req.isAuthenticated())
+        pageId = req.session.passport.user;
+    const userOn = await User.findAll({ where: { admin: pageId, locked: false } });
+    const userOff = await User.findAll({ where: { admin: pageId, locked: true } });
+    const userNull = await User.findAll({ where: { admin: pageId, locked: null } });
+    const userNotNull = await User.findAll({ where: { admin: pageId, locked: { [Op.ne] : null } } });
+
     res.render('manage_user', {
         title: '군미니 홈페이지',
         loggedIn: req.isAuthenticated(),
+        number: {
+            userAll: userNotNull.length + userNull.length,
+            userOn: userOn.length,
+            userOff: userOff.length,
+            userNotNull: userNotNull.length,
+            userNull: userNull.length,
+        },
+        arr: userNotNull.concat(userNull),
     });
 });
 
